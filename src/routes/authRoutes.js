5d6f544eef51692b80abd76a02df41d456c99de3
lucasdakas <lucasperesou@gmail.com>
2026-05-06 17:15:01 -0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+
 const router = express.Router();
 
 const { register, login } = require("../controllers/authController");
 
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: {
+    error: "Muitas tentativas de login. Tente novamente em 15 minutos."
+  }
+});
+
 router.post("/register", register);
-router.post("/login", login);
+router.post("/login", loginLimiter, login);
 
 module.exports = router;
